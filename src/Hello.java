1383561c209770f2d166f90832294d599f5193e6
heeun <phu98@naver.com>
2025-03-06 18:52:17 +0900

    update fori

--- src/Hello.java
+++ src/Hello.java
@@ -8,5 +8,10 @@ public class Hello {
 
         System.out.println("이거 너가 받아서 사용해봐");
 
+        for (int i = 0; i < 4; i++) {
+
+            int number = i;
+            System.out.println(number);
+        }
     }
 }
